95bf3accf2a59df9c92532b8c87761759a483038
rjmh <john.hughes@quviq.com>
2014-06-06 16:56:11 +0200

    x

--- trees.erl
+++ trees.erl
@@ -23,7 +23,7 @@ tree(Lo,Hi) when Hi < Lo ->
 tree(Lo, Hi) ->
   frequency([{1,leaf},
              {2,?LET(X,choose(Lo,Hi),
-                     ?LET({L,R},{node,tree(Lo,X-1),tree(X+1,Hi)},
+                     ?LET({L,R},{tree(Lo,X-1),tree(X+1,Hi)},
                           ?SHRINK({node,L,X,R},
                                    [leaf,L,R])))}]).
 
