1ec1cb718b1280034837bc87e05d93ec085cc7bf
rjmh <john.hughes@quviq.com>
2014-06-06 15:41:39 +0200

    x

---
+++ trees.erl
@@ -0,0 +1,27 @@
+-module(trees).
+-include_lib("eqc/include/eqc.hrl").
+-compile(export_all).
+
+to_list(leaf) ->
+  [];
+to_list({node,L,X,R}) ->
+  to_list(L) ++ [X] ++ to_list(R).
+
+prop_ordered() ->
+  ?FORALL(T,tree(),
+          ordered(to_list(T))).
+
+ordered(Xs) ->
+  lists:sort(Xs) == Xs.
+
+tree() ->
+  ?SIZED(Size,tree(-Size,Size)).
+
+tree(Lo,Hi) when Hi < Lo ->
+  leaf;
+tree(Lo Hi) ->
+  frequency([{1,leaf},
+             {2,?SHRINK(
+                   ?LET(X,choose(Lo,Hi),
+                        {node,tree(Lo,X-1),X,tree(X+1,Hi)}),
+                   [leaf])}]).
