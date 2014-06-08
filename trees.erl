f57b9c22e2f61d70b7cb5a7e87dfdf351fdf4a0c
rjmh <john.hughes@quviq.com>
2014-06-08 22:27:24 +0200

    a

--- trees.erl
+++ trees.erl
@@ -40,28 +40,25 @@ member(X,{node,L,Y,R}) ->
     X==Y ->
       true;
     X<Y ->
-      member(X,L);
+      member(X,R);
     X>Y ->
-      member(X,R)
+      member(X,L)
   end.
 
-%% insert
+%% %% insert
 
-prop_insert() ->
-  ?FORALL({X,T},{nat(),tree()},
-    begin
-      L = to_list(insert(X,T)),
-      conjunction([{ordered,ordered(L)},
-                   {elements,L==lists:umerge([X],to_list(T))}])
-    end).
+%% prop_insert() ->
+%%   ?FORALL({X,T},{nat(),tree()},
+%%     begin
+%%       L = to_list(insert(X,T)),
+%%       L==lists:umerge([X],to_list(T))
+%%     end).
 
-insert(X,leaf) ->
-  {node,leaf,X,leaf};
-insert(X,{node,L,Y,R}) ->
-  if X<Y ->
-      {node,insert(X,L),Y,R};
-     X>Y ->
-      {node,L,Y,insert(X,R)};
-     X==Y ->
-      {node,L,X,R}
-  end.
+%% insert(X,leaf) ->
+%%   {node,leaf,X,leaf};
+%% insert(X,{node,L,Y,R}) ->
+%%   if X<Y ->
+%%       {node,insert(X,L),Y,R};
+%%      X>=Y ->
+%%       {node,L,Y,insert(X,R)}
+%%   end.
