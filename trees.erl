10299d8f1f3ade644690e77a347aa8e2cc32a619
rjmh <john.hughes@quviq.com>
2014-06-09 09:31:47 +0200

    a

--- trees.erl
+++ trees.erl
@@ -59,6 +59,8 @@ insert(X,leaf) ->
 insert(X,{node,L,Y,R}) ->
   if X<Y ->
       {node,insert(X,L),Y,R};
-     X>=Y ->
+     X==Y ->
+      {node,L,X,R};
+     X>Y ->
       {node,L,Y,insert(X,R)}
   end.
